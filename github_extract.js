a68d731c4622684343f717b0886c197818ed94b0
Paul Gege <pgege@Pauls-MacBook-Pro.local>
2021-06-14 10:00:01 -0400

    changed auth Header

--- github_extract.js
+++ github_extract.js
@@ -2,20 +2,13 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 
 const url ='https://api.github.com/repos/pgege/pascal-network/tarball/87ee696'
-const authHeader ="token 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" 
+const authHeader ="token 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" 
 
 const options = {
     headers: {
         Authorization: authHeader
     }
 };
-
-function blobToFile(theBlob, fileName){
-    //A Blob() is almost a File() - it's just missing the two properties below which we will add
-    theBlob.lastModifiedDate = new Date();
-    theBlob.name = fileName;
-    return theBlob;
-}
 
 fetch(url, options)
     .then( res => res.buffer() )
